2b3b8ce9ef50dd573ef43430aaaa243628d120e8
Steve <mittosteve@gmail.com>
2019-10-25 13:02:07 +0300

    updating the nation models

--- neigh1/models.py
+++ neigh1/models.py
@@ -3,6 +3,10 @@ from django.db import models as md
 class Nation(md.Model):
     name = md.CharField(max_length=200)
     flag = md.URLField()
+    calling_codes =md.IntegerField()
+    currency=md.CharField(max_length=100)
+    symbol=md.CharField(max_length=10)
+
 
     class Meta:
         verbose_name = 'nation'
@@ -16,27 +20,30 @@ class Nation(md.Model):
 
 class Town(md.Model):
     town = md.CharField(max_length = 200)
-    nation = md.ForeignKey(Nation,on_delete =md.PROTECT)
+    nation = md.ForeignKey(Nation,on_delete =md.PROTECT , related_name='towns')
 
     class Meta:
         verbose_name = 'town'
         ordering = ['nation']
 
     def __str__(self):
-        return f'{self.town} in {self.nation}'
+        return f'{self.nation} -> {self.town}'
 
     def save(self):
         self.save()
 
 class Neighborhood(md.Model):
     name = md.CharField(max_length=200)
-    location = md.ForeignKey(Town,on_delete=md.CASCADE)
+    location = md.ForeignKey(Town,on_delete=md.CASCADE,related_name='neighborhoods')
     occupants = md.IntegerField()
     registered_occupants = md.IntegerField()
 
     class Meta:
-        verbose_name = 'nation'
+        verbose_name = 'neighborhood'
         ordering = ['name']
 
     def save(self):
         self.save()
+
+    def __str__(self):
+        return self.name
