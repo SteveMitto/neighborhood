48554715495ec0785004b514c67bffbc7a075e52
Steve <mittosteve@gmail.com>
2019-10-25 12:42:28 +0300

    creating country ,neighborhood and town models

--- neigh1/models.py
+++ neigh1/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+from django.db import models as md
 
-# Create your models here.
+class Nation(md.Model):
+    name = md.CharField(max_length=200)
+    flag = md.URLField()
+
+    class Meta:
+        verbose_name = 'nation'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self):
+        self.save()
+
+class Town(md.Model):
+    town = md.CharField(max_length = 200)
+    nation = md.ForeignKey(Nation,on_delete =md.PROTECT)
+
+    class Meta:
+        verbose_name = 'town'
+        ordering = ['nation']
+
+    def __str__(self):
+        return f'{self.town} in {self.nation}'
+
+    def save(self):
+        self.save()
+
+class Neighborhood(md.Model):
+    name = md.CharField(max_length=200)
+    location = md.ForeignKey(Town,on_delete=md.CASCADE)
+    occupants = md.IntegerField()
+    registered_occupants = md.IntegerField()
+
+    class Meta:
+        verbose_name = 'nation'
+        ordering = ['name']
+
+    def save(self):
+        self.save()
