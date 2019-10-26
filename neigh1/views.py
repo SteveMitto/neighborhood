9a3b086d1361c0730104e15cb2f2d18504652068
Steve <mittosteve@gmail.com>
2019-10-26 17:35:31 +0300

    Allowing signup for the users

--- neigh1/views.py
+++ neigh1/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
+    context={
+    "form":form
+    }
+    print("********************")
+    return render(request,'registration/signup.html',context)
 # Create your views here.
