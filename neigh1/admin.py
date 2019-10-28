85a1e7fcc19d6b3a71c109cc4bab7c3fb7fa3d5b
Steve <mittosteve@gmail.com>
2019-10-28 06:17:04 +0300

    Regestering the bussiness model to the admin

--- neigh1/admin.py
+++ neigh1/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile ,Nation,Town,Neighborhood
+from .models import Profile ,Nation,Town,Neighborhood,Bussines
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Nation)
 admin.site.register(Town)
 admin.site.register(Neighborhood)
+admin.site.register(Bussines)
